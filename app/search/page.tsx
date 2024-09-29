48d775a82b5e72e7e3f0221018f365122c3451fe
truonggiang2512 <hanguyentruonggiang004@gmail.com>
2024-09-29 21:42:39 +0700

    merge liked page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import SearchInput from "@/components/SearchInput";
 import React from "react";
 import SearchContent from "./components/SearchContent";
+export const revalidate = 0;
 interface SearchProps {
   searchParams: {
     title: string;
